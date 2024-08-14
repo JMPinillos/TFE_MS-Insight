98b9243075487dc36659d0521169021ba230cf16
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2024-08-14 10:38:25 +0200

    update

--- Code/MS-Insight_Frontend/src/interfaces/TasksResume.ts
+++
@@ -1,6 +0,0 @@
-// Define la interfaz TasksResume para representar un resumen de tareas
-export interface TasksResume {
-    total: number; // Propiedad 'total': número total de tareas
-    completes: number; // Propiedad 'completes': número de tareas que están completadas
-    pending: number; // Propiedad 'pending': número de tareas que están pendientes de completar
-}
